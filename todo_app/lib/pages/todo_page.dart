be10c116fb6af11cf8b46a881d3c5d64238c52af
Aly Badara Coulibaly <alybadara1803@gmail.com>
2026-04-16 07:25:43 +0100

    Update

--- todo_app/lib/pages/todo_page.dart
+++ todo_app/lib/pages/todo_page.dart
@@ -8,15 +8,49 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
+  // Text editing controler to get access to what the user typed
+  TextEditingController myControler = TextEditingController();
+
+  String greetingMessage = "";
+  //method
+  void greetUser() {
+    setState(() {
+      greetingMessage = 'Hello, ' + myControler.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      body: Center(
-        child: TextField(
-          cursorColor: Colors.red,
-          textAlign: TextAlign.center,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(greetingMessage, style: TextStyle(
+                  fontSize: 20,
+                ),),
+                TextField(
+                  textAlign: TextAlign.center,
+                  controller: myControler,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Type your name ..',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: greetUser,
+                  child: Text('Tap')
+                  )
+              ],
+            ),
+          ),
         ),
-        ),
-    ));
+      ),
+    );
   }
 }
+
