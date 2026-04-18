de6509968f92880c0f734fff3814ebe1c9af5390
Aly Badara Coulibaly <alybadara1803@gmail.com>
2026-04-18 20:05:21 +0100

    Update todo_page.dart

--- todo_app/lib/pages/todo_page.dart
+++ todo_app/lib/pages/todo_page.dart
@@ -1,59 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class TodoPage extends StatefulWidget {
-//   const TodoPage({super.key});
-
-//   @override
-//   State<TodoPage> createState() => _TodoPageState();
-// }
-
-// class _TodoPageState extends State<TodoPage> {
-//   // Text editing controler to get access to what the user typed
-//   TextEditingController myControler = TextEditingController();
-
-//   String greetingMessage = "";
-//   //method
-//   void greetUser() {
-//     setState(() {
-//       greetingMessage = 'Hello, ' + myControler.text;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         body: Center(
-//           child: Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text(greetingMessage, style: TextStyle(
-//                   fontSize: 20,
-//                 ),),
-//                 TextField(
-//                   textAlign: TextAlign.center,
-//                   controller: myControler,
-//                   decoration: InputDecoration(
-//                     border: OutlineInputBorder(),
-//                     hintText: 'Type your name ..',
-//                   ),
-//                 ),
-//                 ElevatedButton(
-//                   onPressed: greetUser,
-//                   child: Text('Tap')
-//                   )
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 class TodoPage extends StatefulWidget {
@@ -64,32 +8,50 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
-  //variable
-  String greetMessage = '';
-
+  // Text editing controler to get access to what the user typed
   TextEditingController myControler = TextEditingController();
 
-  //fonction
+  String greetingMessage = "";
+  //method
   void greetUser() {
-    greetMessage = 'Hello' + myControler.text;
+    setState(() {
+      greetingMessage = 'Hello, ' + myControler.text;
+    });
   }
 
-  //UI (user interface)
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(greetMessage),
-              TextField(controller: myControler),
-              ElevatedButton(onPressed: greetUser, child: Text('Tap')),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(greetingMessage, style: TextStyle(
+                  fontSize: 20,
+                ),),
+                TextField(
+                  textAlign: TextAlign.center,
+                  controller: myControler,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Type your name ..',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: greetUser,
+                  child: Text('Tap')
+                  )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
